79421f7b39deb9ed838da4faa6e0c997ff53b130
Kauê Riki Kakinoana <126582273+kaueriki@users.noreply.github.com>
2024-11-19 18:18:50 -0300

    4. Musicas

---
+++ 3. Screenmatch/src/br/com/alura/screenmatch/calculos/FiltroRecomendacao.java
@@ -0,0 +1,15 @@
+package br.com.alura.screenmatch.calculos;
+
+public class FiltroRecomendacao {
+    private String recomencao;
+
+    public void filtra(Classificavel classificavel) {
+        if (classificavel.getClassificacao()>=  4) {
+            System.out.println("Está entre os preferidos do momento");
+        }  else if (classificavel.getClassificacao()>=  2) {
+            System.out.println("muito bem  avaliado no momento");
+        }  else {
+            System.out.println("Coloque na sua lista para assistir  depois");
+        }
+    }
+}
